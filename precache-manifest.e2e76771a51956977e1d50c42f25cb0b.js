f14398e6ba388d9644d0b3733aa6746b32f2d42f
Gabriel Gutierrez <35079758+Gabriel6go8d@users.noreply.github.com>
2020-04-17 19:17:16 +0200

    Updates

---
+++ precache-manifest.e2e76771a51956977e1d50c42f25cb0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c742269301f9ffbe7ec3770abf1a7908",
+    "url": "/curriculum-vitae/index.html"
+  },
+  {
+    "revision": "e46527f2b9025a4a05ae",
+    "url": "/curriculum-vitae/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "795669ac712d371a6570",
+    "url": "/curriculum-vitae/static/css/main.50b01ee5.chunk.css"
+  },
+  {
+    "revision": "e46527f2b9025a4a05ae",
+    "url": "/curriculum-vitae/static/js/2.4203f758.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/curriculum-vitae/static/js/2.4203f758.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "795669ac712d371a6570",
+    "url": "/curriculum-vitae/static/js/main.eb4a17a4.chunk.js"
+  },
+  {
+    "revision": "0cd5c4d799dcc97324bf",
+    "url": "/curriculum-vitae/static/js/runtime-main.59475df7.js"
+  }
+]);
