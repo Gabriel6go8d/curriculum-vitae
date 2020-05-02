8c18282b38c3dec6b9095a2e425b365d05bb6cce
Gabriel Gutierrez <35079758+Gabriel6go8d@users.noreply.github.com>
2020-05-02 17:58:28 +0200

    a

--- src/Components/Section5.js
+++ src/Components/Section5.js
@@ -10,11 +10,16 @@ function Section5() {
                 <h1 className='col-12'>Portafolio</h1>
                 <a href='https://github.com/Gabriel6go8d' className='col-12'>https://github.com/Gabriel6go8d</a>
                 <div className='row justify-content-between'>
+
+                    <Section5A title='API-Wheater' description='Projecto de clima con React y Axios' img='https://bbvaopen4u.com/sites/default/files/styles/big-image/public/img/new/bbva-open4u-que-hace-una-api-por-mi-negocio.png?itok=TSMlrZhi' direction='https://gabriel6go8d.github.io/api-training/'/>
+
+
                     <Section5A title='React' description='Pagina de practica con React y Bootstrap' img='https://i.pinimg.com/originals/5c/7b/53/5c7b53a7be1dd267f24f7559584d1345.jpg' direction='https://gabriel6go8d.github.io/firts-steps/'/>
 
                     <Section5A title='Canvas' description='Pagina de practica para Canvas utilizando React y Bootstrap' img='https://www.loopeando.com/wp-content/uploads/2016/11/html5.jpg' direction='https://gabriel6go8d.github.io/canvas-training/'/>
 
                     <Section5A title='Primeros Pasos' description='Pagina de practica con Laravel y React' img='https://wallpaperaccess.com/full/1567483.jpg' direction='https://github.com/Gabriel6go8d/lorem-page'/>
+                    
                 </div>          
             </div>            
         </React.Fragment>
